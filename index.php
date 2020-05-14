fa628aed7be060cef1865ab723ffb2983aa86eb9
Denis Nyukhalov <denisn2000@gmail.com>
2020-05-14 14:10:43 +0300

    added more html

--- index.php
+++ index.php
@@ -14,5 +14,16 @@
         echo '<h2>' . $date . '</h2>';
     ?>
     </div>
+    <input type="text">
+    <h5></h5>
+    <script>{
+        document.querySelector('input')
+        .addEventListener('input', e => {
+        fetch('/rates.php?dollars=' + e.target.value)
+        .then(x => x.text())
+        .then(x => document.querySelector('h5').textContent = x)
+        .catch(() => document.querySelector('h5').textContent = 'Error');
+        });}
+    </script>
 </body>
 </html>
