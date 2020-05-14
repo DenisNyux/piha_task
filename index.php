96d21863de5ca1fa55dc41c2e2297f92434c1a93
Denis Nyukhalov <denisn2000@gmail.com>
2020-05-14 14:03:04 +0300

    added html

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php
-    require 'vendor/autoload.php';
-    use Carbon\Carbon;
-    echo '<h1>Nyukhalov Denis</h1>';
-    $date = Carbon::now() -> format('j.m.Y h:i');
-    echo '<h2>' . $date . '</h2>';
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <?php
+        require 'vendor/autoload.php';
+        use Carbon\Carbon;
+        echo '<h1>Nyukhalov Denis</h1>';
+        $date = Carbon::now() -> format('j.m.Y h:i');
+        echo '<h2>' . $date . '</h2>';
+    ?>
+    </div>
+</body>
+</html>
