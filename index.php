dcec83cda1cad20d92aecdf7f950ffd593f654ff
Denis Nyukhalov <denisn2000@gmail.com>
2020-05-14 12:31:34 +0300

    edited date

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
     use Carbon\Carbon;
     echo '<h1>Nyukhalov Denis</h1>';
     $date = Carbon::now() -> format('j.m.Y h:i');
-    echo '<h2>' . date . '</h2>';
+    echo '<h2>' . $date . '</h2>';
